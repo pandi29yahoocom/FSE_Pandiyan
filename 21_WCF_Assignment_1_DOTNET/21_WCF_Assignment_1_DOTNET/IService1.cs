4548616b2f2cc74d63b142a56c890651c07203aa
Pandiyan <pandi29@yahoo.com>
2018-11-23 17:20:32 +0530

    WCF :Initial version

---
+++ 21_WCF_Assignment_1_DOTNET/21_WCF_Assignment_1_DOTNET/IService1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _21_WCF_Assignment_1_DOTNET
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string SayHello(string name);
+
+        [OperationContract]
+        string TodayProgram(string name);
+
+        [OperationContract]
+        List<Jobs> OpeningJobs();
+
+        [OperationContract]
+        List<Jobs> OpeningJobsByRole(string role);
+
+        [OperationContract]
+        int Addition(int num, int num2);
+
+        [OperationContract]
+        int Subtraction(int num, int num2);
+
+
+
+    } 
+   
+    [DataContract]
+    public class Jobs
+    {
+        string role = "";
+        string jobTitle = "Hello ";
+        [DataMember]
+        public string Role
+        {
+            get { return role; }
+            set { role = value; }
+        }
+
+        [DataMember]
+        public string JobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+        public List<Jobs> GetJobByRole(string role)
+        {
+            List<Jobs> job = GetAllJob().Where(u => u.Role == role).ToList();
+            return job;
+        }
+        public List<Jobs> GetAllJob()
+        {
+            return new List<Jobs>(){ new Jobs() {JobTitle=".Net Developer",Role="Junior Developer" },
+                new Jobs() { JobTitle = ".Net Developer", Role = "Senior Developer" },
+                 new Jobs() { JobTitle = ".Java Developer", Role = "Junior Developer" },
+                  new Jobs() { JobTitle = "Java Developer", Role = "Senior Developer" },
+                    new Jobs() { JobTitle = "Manager", Role = "Manager" },
+                      new Jobs() { JobTitle = "Manager", Role = "Director" }
+            };
+        }
+    }
+}
---
+++ 21_WCF_Assignment_1_DOTNET/21_WCF_Assignment_1_DOTNET/Service1.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _21_WCF_Assignment_1_DOTNET
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public string SayHello(string name)
+        {
+            if (DateTime.Now.Hour < 12)
+                return "Good Morning " + name;
+            else if (DateTime.Now.Hour < 17)
+                return "Good Afternoon " + name;
+            else
+                return "Good Evening " + name;            
+        }
+        public string TodayProgram(string name)
+        {
+            if ((DateTime.Now.DayOfWeek == DayOfWeek.Saturday) && (DateTime.Now.DayOfWeek == DayOfWeek.Sunday))
+                return "Happy weekend " + name;
+            else
+                return "Enjoy Working day " + name;
+        }
+        public List<Jobs> OpeningJobs()
+        {
+            Jobs job = new Jobs();
+            return job.GetAllJob();
+        }
+
+        public List<Jobs> OpeningJobsByRole(string role)
+        {
+            Jobs job = new Jobs();
+            return job.GetJobByRole(role);
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+    
+}
